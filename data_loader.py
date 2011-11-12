2fa37c797ba2645218b6b83b2500148a8c85eba9
Thomas Kern <thk.kern@gmail.com>
2011-11-12 13:11:43 +0100

    removed all experimental code lines from DataLoader

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,7 @@
 import csv
 
 class DataLoader:
+
   def write_to_disk(self, writer,typ,x,y):
     writer.writerow(["x","y","type"])
     for i in range(len(x)):
@@ -19,7 +20,6 @@ class DataLoader:
   def get_data(self):
     y_all = []
 
-    #x, y = self.generate_random_x_y()
     x, y = self.read_csv("apple_out_rel.csv")
     x, y1 = self.read_csv("bmw_out_rel.csv")
     x, y2 = self.read_csv("google_out_rel.csv")
@@ -30,6 +30,7 @@ class DataLoader:
     x, y7 = self.read_csv("yahoo_out_rel.csv")
     x, y8 = self.read_csv("cabot_out_rel.csv")
     x, y9 = self.read_csv("france_telekom_out_rel.csv")
+
     y_all.append(y5)
     y_all.append(y)
     y_all.append(y8)
@@ -40,24 +41,8 @@ class DataLoader:
     y_all.append(y4)
     y_all.append(y6)
     y_all.append(y7)
+
     labels = ["Agu", "Apple", "Cabot", "BMW", 'France Telekom', 'Google', 'Goldman', 'Shell',  "Staples", "Yahoo"]
-    #writer = csv.writer(open("data.csv","w"), delimiter = ',')
-    #self.write_to_disk(writer,"",x,y)
-
-    #y_all = [y4]
-    #y_all = [
-        #[-2 , -5 , 1 , -1 , 2 , 5 , 6 , -1 , -9]]
-    #x = [0  , 1  , 2 , 3  , 4 , 5 , 6 , 7  , 8]
-
-    #for i in range(n):
-      #x1,y1 = self.generate_random_x_y()
-      #y_all.append(y1)
-      #y_all.append(y)
-
-    #x1,y1 = self.generate_random_x_y(y_max=3)
-    #y_all.append(y1)
-    #n += 1
-    #print(y_all)
 
     return x,y_all, labels
 
